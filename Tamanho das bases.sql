a56599ee97736259fa3f90571ccfd22b3b043d78
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-07-19 16:49:50 -0300

    Update Tamanho das bases.sql

--- Tamanho das bases.sql
+++ Tamanho das bases.sql
@@ -1,6 +1,5 @@
-SELECT      sys.databases.name AS [Database Name], 
- GETDATE() AS [Data],
-        CONVERT(VARCHAR,SUM(size)*8/1024) AS [SizeMB]  
+SELECT      sys.databases.name AS [Banco], 
+        CONVERT(VARCHAR,SUM(size)*8/1024) AS [Tamanho em MB]  
      FROM        sys.databases   
      JOIN        sys.master_files  
      ON          sys.databases.database_id=sys.master_files.database_id  
